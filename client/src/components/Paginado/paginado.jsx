f2b0dc92b94925abbafa90eb5b11a705ef5122d8
Littyfever <valen.morardo18@gmail.com>
2022-09-23 02:00:50 -0300

    prueba

--- client/src/components/Paginado/paginado.jsx
+++ client/src/components/Paginado/paginado.jsx
@@ -16,6 +16,7 @@ const paginado = ({pokemonesPerPage, allPokemones, paginado}) => {
                     pageNumbers &&  pageNumbers.map(number => (
                         <li className="number" key={number}>
                             <a onClick={()=> paginado(number) }>{number}</a>
+                            <h4> coltaaaa</h4>
                         </li>
                     ))
                 }
